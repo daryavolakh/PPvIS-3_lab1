e71f19f4d0b64eda08762fdb6ad04e59c07801e0
Darya <dasha27061999@mail.ru>
2018-09-27 13:37:42 +0300

    add prisoner's move

--- src/view/BottleField.java
+++ src/view/BottleField.java
@@ -1,29 +1,71 @@
 package view;
 
 import controller.Controller;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import javafx.scene.shape.*;
+import javafx.animation.*;
+import javafx.scene.text.Text;
 
-public class BottleField{
-	public int height = 300, width = 500;
-	public Canvas canvas;
-	public Controller controller;	
-	
-	public BottleField(Controller controller)
-	{
+public class BottleField {
+	public int height = 400, width = 600;
+	public Pane canvas;
+	public Controller controller;
+	public Color color;
+	public Circle prisonerCircle;
+	public Circle securityCircle;
+
+
+	public BottleField(Controller controller) {
 		this.controller = controller;
+		canvas = new Pane();
+		canvas.setStyle("-fx-background-color: gray;");
+		canvas.setPrefSize(width, height);
+
+		Rectangle door = new Rectangle(20,100, Color.ANTIQUEWHITE);
+		canvas.getChildren().add(door);
 		
-		canvas = new Canvas();
-		canvas.setHeight(height);
-        canvas.setWidth(width);
+		Circle securityCircle = new Circle(20, Color.DARKRED);
+		securityCircle.relocate(40, 200);
+		startSecurityMove(securityCircle);
+		
+		prisonerCircle = new Circle(20, Color.PALEVIOLETRED);
+		prisonerCircle.relocate(500, 200);
+		canvas.getChildren().add(prisonerCircle);
+	}
+
+	public void startSecurityMove(Circle securityCircle) {		
+		canvas.getChildren().add(securityCircle);
+		securityCircleMove(securityCircle, 220);
+
+	}
+
+	public void securityCircleMove(Circle security, int end) {
+		
+		Polyline line = new Polyline();
+	
+		line.getPoints().addAll(new Double[]{0.0,0.0, 0.0,250.0, 0.0,-190.0, 0.0,0.0});
+		
+		PathTransition translation = new PathTransition();
+		
+		translation.setNode(security);
+		translation.setDuration(Duration.seconds(3));
+		translation.setPath(line);
+		translation.setCycleCount(PathTransition.INDEFINITE);
+		
+		translation.play();
 	}
 	
-	
-	public Canvas getCanvas()
+	public Circle getPrisoner()
 	{
+		return prisonerCircle;
+	}
+
+	public Pane getCanvas() {
 		return canvas;
 	}
-	
-	
+
 }
--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,67 +1,107 @@
 package view;
+
 import controller.Controller;
 import javafx.stage.Stage;
+import javafx.scene.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.geometry.Insets;
-
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.event.EventHandler;
+import javafx.event.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 public class MainWindow {
 	public Controller controller;
-	public Stage stage; 
+	public Stage stage;
 	public Button buttonStart = new Button("START");
 	public Label labelLevel = new Label("  level: ");
 	public Label levelNum = new Label("0");
 	public BottleField bottleField;
-	
+
 	public MainWindow(Controller controller)
 	{
 		this.controller = controller;
 		stage = new Stage();
 		stage.setTitle("RUN!!!");
 		stage.setWidth(800);
-		stage.setHeight(600);
+		stage.setHeight(555);
+		stage.setResizable(false);
 		
 		VBox buttonsPane = new VBox();
-		buttonsPane.setPadding(new Insets(10,10,10,10));
+		buttonsPane.setPadding(new Insets(15,15,15,15));
 		
-		buttonStart.setPrefSize(100, 50);
-		labelLevel.setPrefSize(50, 50);
-		levelNum.setPrefSize(50, 50);
+		HBox levelValue = new HBox();
+		levelValue.getChildren().addAll(labelLevel,levelNum);
 		
-		buttonsPane.getChildren().addAll(buttonStart,labelLevel,levelNum);
+		buttonsPane.getChildren().addAll(buttonStart,levelValue);
 		
 		HBox pane = new HBox();
 		bottleField = new BottleField(controller);
 		pane.getChildren().addAll(bottleField.getCanvas(),buttonsPane);
 
 		Scene scene=new Scene(pane);
-		stage.setScene(scene);			
+		stage.setScene(scene);	
+		
+		/*scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+	        @Override
+	        public void handle(KeyEvent event) {
+	            if (event.getCode() == KeyCode.LEFT){
+	            	bottleField.getPrisoner().setTranslateX(bottleField.getPrisoner().getTranslateX() - 5);
+	            }
+	            
+	        }
+	    });*/
+		
+		scene.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCharacter().equals("a"))
+                {
+                	bottleField.getPrisoner().setTranslateX(bottleField.getPrisoner().getTranslateX() - 5);
+                }
+                
+                if (ke.getCharacter().equals("d"))
+                {
+                	bottleField.getPrisoner().setTranslateX(bottleField.getPrisoner().getTranslateX() + 5);
+                }
+                
+                if (ke.getCharacter().equals("w"))
+                {
+                	bottleField.getPrisoner().setTranslateY(bottleField.getPrisoner().getTranslateY() - 5);
+                }
+                
+                if (ke.getCharacter().equals("s"))
+                {
+                	bottleField.getPrisoner().setTranslateY(bottleField.getPrisoner().getTranslateY() + 5);
+                }
+            }
+		});
+		            
 	}
-	
-	public void show()
-	{
+
+	public void show() {
 		stage.show();
 	}
 
-	public void changeLevel()
-	{
+	public void changeLevel() {
 		getLevel();
 		setLevel();
 	}
-	
-	public int getLevel()
-	{
+
+	public int getLevel() {
 		int level = Integer.parseInt(levelNum.getText());
 		return level;
 	}
-	
-	public void setLevel()
-	{
+
+	public void setLevel() {
 		int prevValue = getLevel();
 		levelNum.setText(String.valueOf(prevValue + 1));
 	}
+
+	
 }
